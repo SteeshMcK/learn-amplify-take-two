9e11c1f0c6932cc08cabc95d1408cd0f6c309919
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-15 20:04:10 -0800

    add question component and functionality

---
+++ src/components/Question/Question.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './Question.css'
+
+const initQuestionState = { name: '' }
+const showQuestionState = { questionShown: true }
+
+const Question  = () => {
+    const [userName, setUserName] = useState(initQuestionState)
+    const [questionState, setQuestionState] = useState(showQuestionState)
+   
+    const showGreeting = () => {
+        setQuestionState({
+            questionShown: false
+        })
+    }
+    const handleInputChange = e => {
+        const {name, value} = e.target
+        setUserName({...userName, [name]: value})
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(userName.name)
+        showGreeting()
+    }
+    return (
+        <div className='question'>
+        { 
+            questionState.questionShown
+            ? ( <form id='questionForm' onSubmit={handleSubmit}>
+                    <label>What is your name?
+                        <br />
+                        <input type='text' id='nameField' name='name' placeholder='name' value={userName.name} required onChange={handleInputChange}/>
+                    </label>
+                    <br />
+                    <input type="submit" id='submitBtn' value="SUBMIT NAME" />
+                </form>)
+            : ( <div className='greetingsYall'>
+                    <p>Welcome, {userName.name}!</p>
+               </div>)
+        }
+        </div> 
+    )
+}
+
+export default Question
