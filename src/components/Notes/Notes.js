c93be47dff64059587fde51a4844cce46823cd6a
SteeshMcK <stesha.norstrom@pcc.edu>
2021-01-21 14:53:54 -0800

    create GraphQL API and database

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,7 +1,62 @@
+import React, { useState, useEffect } from 'react';
+// import './Notes.css';
+import { API } from 'aws-amplify';
+import { listTodos } from '../../graphql/queries';
+import { createTodo, deleteTodo } from '../../graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
+
 const Notes = () => {
+    const [notes, setNotes] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchNotes();
+      }, []);
+    
+    async function fetchNotes() {
+        const apiData = await API.graphql({ query: listTodos });
+        setNotes(apiData.data.listTodos.items);
+    }
+
+    async function createNote() {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({ query: createTodo, variables: { input: formData } });
+        setNotes([ ...notes, formData ]);
+        setFormData(initialFormState);
+    }
+
+    async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteTodo, variables: { input: { id } }});
+    }
+
     return (
         <div className='notes'>
-            <h1>Notes go here, y'all!</h1>
+            <h1>My Notes App</h1>
+            <input
+                onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+                placeholder="Note name"
+                value={formData.name}
+            />
+            <input
+                onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+                placeholder="Note description"
+                value={formData.description}
+            />
+            <button onClick={createNote}>Create Note</button>
+            <div style={{marginBottom: 30}}>
+                {
+                notes.map(note => (
+                    <div key={note.id || note.name}>
+                    <h2>{note.name}</h2>
+                    <p>{note.description}</p>
+                    <button onClick={() => deleteNote(note)}>Delete note</button>
+                    </div>
+                ))
+                }
+            </div>
         </div>
     )
 }
